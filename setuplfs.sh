ac2ffd1716dcd39aaa940fab7fdc6898ffda7bb2
Maximilian Fridrich <maxi@fridrich.at>
2018-03-18 19:20:51 -0500

    Added chapter 6.7 to 6.10

--- setuplfs.sh
+++ setuplfs.sh
@@ -784,6 +784,164 @@ ch6_4 () {
     /tools/bin/bash --login +h
 }
 
+ch6_7 () {
+    echo "6.7. Linux-4.15.3 API Headers"
+
+	cd /sources
+	tar -xf linux-4.15.3.tar.xz
+	cd linux-4.15.3
+
+    make mrproper
+    make INSTALL_HDR_PATH=dest headers_install
+    find dest/include \( -name .install -o -name ..install.cmd \) -delete
+    cp -rv dest/include/* /usr/include
+
+	cd ..
+	rm -rf linux-4.15.3
+}
+
+ch6_8 () {
+    echo "6.8. Man-pages-4.15"
+
+	cd /sources
+	tar -xf man-pages-4.15.tar.xz
+	cd man-pages-4.15
+
+    make install
+
+	cd ..
+	rm -rf man-pages-4.15
+}
+
+ch6_9 () {
+    echo "6.9. Glibc-2.27"
+
+	cd /sources
+	tar -xf glibc-2.27.tar.xz
+	cd glibc-2.27
+
+    patch -Np1 -i ../glibc-2.27-fhs-1.patch
+    ln -sfv /tools/lib/gcc /usr/lib
+    case $(uname -m) in
+        i?86)    GCC_INCDIR=/usr/lib/gcc/$(uname -m)-pc-linux-gnu/7.3.0/include
+                ln -sfv ld-linux.so.2 /lib/ld-lsb.so.3
+        ;;
+        x86_64) GCC_INCDIR=/usr/lib/gcc/x86_64-pc-linux-gnu/7.3.0/include
+                ln -sfv ../lib/ld-linux-x86-64.so.2 /lib64
+                ln -sfv ../lib/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
+        ;;
+    esac
+    rm -f /usr/include/limits.h
+    mkdir -v build
+    cd       build
+    CC="gcc -isystem $GCC_INCDIR -isystem /usr/include" \
+    ../configure --prefix=/usr                          \
+                 --disable-werror                       \
+                 --enable-kernel=3.2                    \
+                 --enable-stack-protector=strong        \
+                 libc_cv_slibdir=/lib
+    unset GCC_INCDIR
+    make
+    make check
+    touch /etc/ld.so.conf
+    sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
+    make install
+    cp -v ../nscd/nscd.conf /etc/nscd.conf
+    mkdir -pv /var/cache/nscd
+    mkdir -pv /usr/lib/locale
+    localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
+    localedef -i de_DE -f ISO-8859-1 de_DE
+    localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
+    localedef -i de_DE -f UTF-8 de_DE.UTF-8
+    localedef -i en_GB -f UTF-8 en_GB.UTF-8
+    localedef -i en_HK -f ISO-8859-1 en_HK
+    localedef -i en_PH -f ISO-8859-1 en_PH
+    localedef -i en_US -f ISO-8859-1 en_US
+    localedef -i en_US -f UTF-8 en_US.UTF-8
+    localedef -i es_MX -f ISO-8859-1 es_MX
+    localedef -i fa_IR -f UTF-8 fa_IR
+    localedef -i fr_FR -f ISO-8859-1 fr_FR
+    localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
+    localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
+    localedef -i it_IT -f ISO-8859-1 it_IT
+    localedef -i it_IT -f UTF-8 it_IT.UTF-8
+    localedef -i ja_JP -f EUC-JP ja_JP
+    localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
+    localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
+    localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
+    localedef -i zh_CN -f GB18030 zh_CN.GB18030
+    make localedata/install-locales
+
+    echo "6.9.2. Configuring Glibc"
+    cat > /etc/nsswitch.conf << "EOF"
+# Begin /etc/nsswitch.conf
+
+passwd: files
+group: files
+shadow: files
+
+hosts: files dns
+networks: files
+
+protocols: files
+services: files
+ethers: files
+rpc: files
+
+# End /etc/nsswitch.conf
+EOF
+    tar -xf ../../tzdata2018c.tar.gz
+    ZONEINFO=/usr/share/zoneinfo
+    mkdir -pv $ZONEINFO/{posix,right}
+    for tz in etcetera southamerica northamerica europe africa antarctica  \
+              asia australasia backward pacificnew systemv; do
+        zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz}
+        zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz}
+        zic -L leapseconds -d $ZONEINFO/right -y "sh yearistype.sh" ${tz}
+    done
+    cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
+    zic -d $ZONEINFO -p America/New_York
+    unset ZONEINFO
+    cp -v /usr/share/zoneinfo/America/North_Dakota/Center /etc/localtime
+    cat > /etc/ld.so.conf << "EOF"
+# Begin /etc/ld.so.conf
+/usr/local/lib
+/opt/lib
+
+EOF
+   cat >> /etc/ld.so.conf << "EOF"
+# Add an include directory
+include /etc/ld.so.conf.d/*.conf
+
+EOF
+    mkdir -pv /etc/ld.so.conf.d 
+
+	cd ../..
+	rm -rf glibc-2.27
+}
+
+ch6_10 () {
+    echo "6.10. Adjusting the Toolchain"
+
+    mv -v /tools/bin/{ld,ld-old}
+    mv -v /tools/$(uname -m)-pc-linux-gnu/bin/{ld,ld-old}
+    mv -v /tools/bin/{ld-new,ld}
+    ln -sv /tools/bin/ld /tools/$(uname -m)-pc-linux-gnu/bin/ld
+    gcc -dumpspecs | sed -e 's@/tools@@g'                   \
+        -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
+        -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >      \
+        `dirname $(gcc --print-libgcc-file-name)`/specs
+    echo 'int main(){}' > dummy.c
+    cc dummy.c -v -Wl,--verbose &> dummy.log
+    readelf -l a.out | grep ': /lib'
+    grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
+    grep -B1 '^ /usr/include' dummy.log
+    grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
+    grep "/lib.*/libc.so.6 " dummy.log
+    grep found dummy.log
+    rm -v dummy.c a.out dummy.log
+}
+
 #ch5_4
 #ch5_5
 #ch5_6
@@ -818,4 +976,8 @@ ch6_4 () {
 #ch5_35
 #ch5_36
 #ch6_2
-ch6_4
+#ch6_4
+#ch6_7
+#ch6_8
+#ch6_9
+ch6_10
